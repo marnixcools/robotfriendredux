1f97cd21f9c574aae32a69515208950d1e5144b1
MarnixCools <marnixcools2@msn.com>
2020-07-21 19:01:59 +0200

    Werkt terug ok met CardList

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-import Card from './Card';
+import CardList from './CardList.js';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from './robots';
@@ -10,13 +9,8 @@ import { robots } from './robots';
 ReactDOM.render(
   <React.StrictMode>
   	<div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
-    <Card id={robots[4].id} name={robots[4].name} email={robots[4].email}/>
-    <Card id={robots[5].id} name={robots[5].name} email={robots[5].email}/>
-    </div>,
+  		<CardList robots={robots}/>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
